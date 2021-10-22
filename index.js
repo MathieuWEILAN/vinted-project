bc0d897cd30e061a6ba843d421cbb70f9ec011fc
Mathieu WEI LAN <ti3uxx@air-de-mathieu.lan>
2021-10-22 14:24:20 +0200

    changement api secret

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const cors = require("cors");
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAM,
   api_key: process.env.CLOUDINNARY_API_KEY,
-  api_secret: CLOUDINARY_API_SECRET,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
